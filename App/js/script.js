07e20efda7baf57ab1877ce1ca3d1e68d46a8374
fiser-luongNT7 <luongnt95@gmail.com>
2016-04-17 09:01:17 +0700

    Update script.js

--- App/js/script.js
+++ App/js/script.js
@@ -42,19 +42,19 @@
 
 	});
 	var timer = null;
-	var interval = 1000,time,value = 0,sec = 0, min =0, hour =0;
+	var interval = 10,time,value = 0,sec = 0, min =0, hour =0;
 
   
   
-function timeToString(sec_num) {   
-    var hours   = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+function timeToString(heartbits_num) {   
+    var minutes   = Math.floor(heartbits_num / 6000);
+    var seconds = Math.floor((heartbits_num - (minutes * 6000)) / 100);
+    var heartbits = heartbits_num - (minutes * 6000) - (seconds * 100);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
+    if (minutes   < 10) {minutes   = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+seconds;}
-    time    = hours + minutes + seconds;
+    if (heartbits < 10) {heartbits = "0"+heartbits;}
+    time    = minutes + seconds + heartbits;
     return time;
 }
 function setDigits(_value) {
